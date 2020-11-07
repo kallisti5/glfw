44b2bc3b960eb13389ea67409484c5bd8411785f
Alexander von Gluck IV <kallisti5@unixzen.com>
2020-11-07 16:06:14 +0000

    [WIP] hgl: Support for Haiku

--- src/internal.h
+++ src/internal.h
@@ -190,6 +190,8 @@ typedef void (APIENTRY * PFN_vkVoidFunction)(void);
  #include "wl_platform.h"
 #elif defined(_GLFW_OSMESA)
  #include "null_platform.h"
+#elif defined(_GLFW_HGL)
+ #include "hgl_platform.h"
 #else
  #error "No supported window creation API selected"
 #endif
